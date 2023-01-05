df44b7f476415dccf92880c0e806e5e47126336f
Wesley <wesley1471@gmail.com>
2023-01-04 22:04:52 -0300

    Saved delete all

--- CourseEx/Program.cs
+++ CourseEx/Program.cs
@@ -6,13 +6,7 @@ namespace CourseEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre a largura e altura do retângulo: ");
-            Retangulo r = new Retangulo();
-            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("PERÍMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+            
         }
     }
 }
