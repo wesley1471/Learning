8261e4933cdafd5eb2b3348534083a50fbede5bd
Wesley <wesley1471@gmail.com>
2023-01-04 22:53:54 -0300

    Finished this

--- CourseEx/Program.cs
+++ CourseEx/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Globalization;
+using CourseEx.Entities;
+using CourseEx.Entities.Enums;
 namespace CourseEx
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Order order = new Order
+            {
+                id = 1800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            Console.WriteLine(order);
+            Console.WriteLine(txt);
         }
     }
 }
