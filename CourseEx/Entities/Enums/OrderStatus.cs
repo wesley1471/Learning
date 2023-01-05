77df039648a949946204617e90231140d9272076
Wesley <wesley1471@gmail.com>
2023-01-04 22:16:40 -0300

    Order Status progress

--- CourseEx/Entities/Enums/OrderStatus.cs
+++ CourseEx/Entities/Enums/OrderStatus.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace CourseEx.Entities.Enums
 {
-    internal class OrderStatus
+    enum OrderStatus : int
     {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
     }
 }
--- CourseEx/Entities/Order.cs
+++ CourseEx/Entities/Order.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using CourseEx.Entities.Enums;
 namespace CourseEx.Entities
 {
     internal class Order
     {
+        public int id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return id + ", " + Moment + ", " + Status;
+        }
+
     }
 }
