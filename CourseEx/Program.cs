20825bca9658482cbeb77cc0444477da31ac192b
Wesley <wesley1471@gmail.com>
2023-01-04 22:02:32 -0300

    First Commit and Here begins

---
+++ CourseEx/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace CourseEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            Retangulo r = new Retangulo();
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
